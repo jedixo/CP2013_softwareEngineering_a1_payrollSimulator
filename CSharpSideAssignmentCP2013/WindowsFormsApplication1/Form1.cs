66e8a111d922f8b436b982032d924c7f89637a36
jedixo <jake.dixon@my.jcu.edu.au>
2015-08-26 19:17:36 +1000

    c sharp added

---
+++ CSharpSideAssignmentCP2013/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = System.IO.File.ReadAllText(@"test.txt");
+            System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+            string[] lines = System.IO.File.ReadAllLines(@"test.txt");
+            System.Console.WriteLine("Contents of WriteLines2.txt = ");
+            foreach (string line in lines)
+            {
+                // Use a tab to indent each line of the file.
+                Console.WriteLine(line);
+            }
+
+        }
+
+    }
+}
