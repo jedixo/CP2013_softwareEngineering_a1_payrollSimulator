6e92faeb084fcd6089bf9dfd4a96746b5c946c55
B_line <blake.hobbs@my.jcu.au>
2015-10-12 15:33:12 +1100

    Fixed some CSS + dynamic options
    Fixed css issues on home page
    Added drop down menus to add employee menu
    View timecard/ sales card appears depeding on employee

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link href="main.css" rel="stylesheet" type="text/css">
+<meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1">
+</head>
+
+<body>
+<a href="index.php">
+<header>PleasurePay</header>
+</a>
+<a href="newEmployee.php">
+<div class="outer">
+        <div class="image">
+        
+        </div>
+        <p> add new employee</p>
+</div>
+</a>
+<a href="viewEmp.php">
+    <div class="outer">
+      <div class="image">
+
+            </div>
+            <p> view employee</p>
+    </div>
+</a>
+<div class="outer">
+        <div class="image">
+        
+        </div>
+        <p> promote employee</p>
+    </div>
+    <div class="outerpay">
+        <div class="image">
+        
+        </div>
+        <p> Pay employee</p>
+    </div>
+</body>
+</html>
---
+++ viewEmp.php
@@ -0,0 +1,66 @@
+<?php
+include('empDbConnect.php');
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>PHP SQLite Database Example (Phone Records)</title>
+<link href="main.css" rel="stylesheet" type="text/css">
+</head>
+    <body>
+<a href="index.php">
+<header>PleasurePay</header>
+</a>
+<h1>Employee Database:</h1>
+        <br>
+        <form id='addForm' name='addForm' method='post' action='empProcess.php'><input type="button" name='Add' value='Add Employee'/></form>
+<table border="1">
+    <tr>
+        <th>id:</th>
+        <th>First:</th>
+        <th>Last:</th>
+        <th>Address:</th>
+        <th>Pay Type:</th>
+        <th>Pay Delivery:</th>
+        <th>Union:</th>
+        <th>Salary:</th>
+        <th>Options:</th>
+    </tr>
+
+<?php
+$sql = "SELECT * FROM employees";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+
+        echo "<form id='deleteForm' name='deleteForm' method='post' action='empProcess.php'><tr><td>" . $row["id"] . "</td>"; 
+        echo "<td>" . $row["first_name"] . "</td>";
+        echo "<td>" . $row["last_name"] . "</td>";
+        echo "<td>" . $row["Address"] . "</td>";
+        switch($row["pay_type"]){
+            case 0: echo "<td>" . Hourly . "</td>"; break;
+            case 1: echo "<td>" . Salary . "</td>"; break;
+            case 2: echo "<td>" . Comission . "</td>"; break;}
+        switch($row["pay_delivery"]){
+            case 0: echo "<td>" . Mail . "</td>" ;break;
+            case 1: echo "<td>" . Pickup . "</td>";break;
+            case 2: echo "<td>" . "Direct deposit" . "</td>";break;}
+        echo "<td>" . $row["Union"] . "</td>";
+        echo "<td>" . $row["Salary"] . "</td>";
+        ?>
+        <td><input type="hidden" name="id" value="<?php echo $row["id"]; ?>"/>
+            <input type="submit" name="submit" value="Update Entry" />
+<input type="submit" name="submit" value="X" class="deleteButton"></td></tr></form>
+    <?php
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+    
+</table>
+</html>
